4cfdc015f6a8fdae46942e52aa38ad6030be5ddd
Никита Комаров <nikitakmrv76@gmail.com>
2025-03-22 17:27:15 +0300

    Подготовка проекта к работе

---
+++ prisma/seed.ts
@@ -0,0 +1,17 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+async function main() {
+  await Promise.all([
+    prisma.game.create({ data: { name: "game-1" } }),
+    prisma.game.create({ data: { name: "game-2" } }),
+  ]);
+}
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
